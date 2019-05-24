910381676fdf8fc06ce2845a6f822c6746f37f69
Anshu Saxena <anshu@so.city>
2019-05-24 19:13:57 +0530

    javascript drum roll demo

---
+++ ScriptFiles/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;  //stop the function from running all together
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+})
